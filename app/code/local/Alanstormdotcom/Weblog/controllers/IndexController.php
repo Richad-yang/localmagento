9c4d71b62b0397bede69231b1a61e35787cf0a9e
richard <richard@localhost>
2016-12-16 16:04:36 +0800

    #localmagento-10000 *add model

--- app/code/local/Alanstormdotcom/Weblog/controllers/IndexController.php
+++ app/code/local/Alanstormdotcom/Weblog/controllers/IndexController.php
@@ -8,12 +8,36 @@ class Alanstormdotcom_Weblog_IndexController extends Mage_Core_Controller_Front_
 		$blogpost = Mage::getModel('weblog/blogpost');
 		echo("Loading the blogpost with an ID of ".$params['id']);
 		$blogpost->load($params['id']);
-		$data = $blogpost->getData();
+		//$blogpost->setTitle('Code Post!');
+		$data = $blogpost->getTitle();
 		var_dump($data);
-		$tille = $blogpost->getData('title');
-		$origTitle = $blogpost->getOrigData('title');
-		var_dump($tille);echo "<br />";
-		var_dump($origTitle);
+	}
+	public function createNewPostAction() {
+		$blogpost = Mage::getModel('weblog/blogpost');
+		$blogpost->setTitle('Code Post!');
+		$blogpost->setPost('This post was created from code!');
+		$blogpost->save();
+		echo 'post created';
+	}
+	public function editFirstPostAction() {
+		$blogpost = Mage::getModel('weblog/blogpost');
+		$blogpost->load(1);
+		$blogpost->setTitle("The First post!");
+		$blogpost->save();
+		echo 'post edited';
+	}
+	public function deleteFirstPostAction() {
+		$blogpost = Mage::getModel('weblog/blogpost');
+		$blogpost->load(1);
+		$blogpost->delete();
+		echo 'post removed';
+	}
+	public function showAllBlogPostsAction() {
+		$posts = Mage::getModel('weblog/blogpost')->getCollection();
+		foreach($posts as $blog_post){
+			echo '<h3>'.$blog_post->getTitle().'</h3>';
+			echo nl2br($blog_post->getPost());
+		}
 	}
 }
 
