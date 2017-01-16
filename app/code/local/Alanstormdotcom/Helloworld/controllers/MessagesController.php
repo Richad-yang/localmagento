f030ec2969ebf5439fdd95d33c7dfbee3db97c53
richard <richard@localhost>
2017-01-16 18:26:34 +0800

    cuntom magento system configuration

--- app/code/local/Alanstormdotcom/Helloworld/controllers/MessagesController.php
+++ app/code/local/Alanstormdotcom/Helloworld/controllers/MessagesController.php
@@ -4,5 +4,25 @@ class Alanstormdotcom_Helloworld_MessagesController extends Mage_Core_Controller
 	{
 		echo 'Another Goodbye';
 	}
+	
+	public function ShowXmlAction()
+	{
+		//header('Content-Type: text/xml');
+		header('Content-Type: text/plain');
+		echo $config = Mage::getConfig()
+		->loadModulesConfiguration('system.xml')
+		->getNode()
+		->asXML();
+		exit;
+	}
+	public function getconfigAction()
+	{
+		$config = Mage::getStoreConfig('web/unsecure/base_url');
+		if ($config){
+			echo $config;
+		}else{
+			echo 'no data';
+		}
+	}
 }
 ?>
